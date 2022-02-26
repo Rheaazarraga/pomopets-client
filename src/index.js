e682c147364903ac1e7fc2e334be5df3312c85f3
Arushi Katyal <katy.arushi@gmail.com>
2022-02-25 19:47:54 -0500

    add netlify config

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import App from './App';
 // styles
 import './styles/App.scss';
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 const rootElement = document.getElementById("root");
 render(
   <App/>,
