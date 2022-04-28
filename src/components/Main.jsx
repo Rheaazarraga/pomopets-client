7bd5ffd096b695d426bb13c8e97037d96db0b90b
Arushi Katyal <katy.arushi@gmail.com>
2022-04-28 18:45:09 -0400

    change href for animated egg

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 import egg from "../assets/eggs/egg.png";
 
-export default function Main(props) {
+export default function Main() {
   return (
-    <>
-      <div className="body pt-5">
-        <div className="home main-container">
-          <div className="text-container">
-            <h1 className="logo-text"> PomoPets </h1>
-            <br />
-            <h3>
-              <p className="blurb">
-                PomoPets is an incentivized Pomodoro timer. Begin your focus
-                session with a mysterious, hatchable egg and upon completing
-                your focus session, you are rewarded with a cute digital pet!{" "}
-              </p>
-              <br />
-              <p className="blurb">
-                Pomodoro sessions are typically 25 minutes in length, with a 5
-                minute break between sessions. After every few sessions, take a
-                longer break to recharge and refresh your mind!{" "}
-              </p>
-            </h3>
-          </div>
-          <a href="/timers">
-            <img
-              src={egg}
-              alt="egg-on-homepage"
-              className="egg"
-              width="400px"
-              height="400px"
-            />
-          </a>
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className='body pt-5'>
+				<div className='home main-container'>
+					<div className='text-container'>
+						<h1 className='logo-text'> PomoPets </h1>
+						<br />
+						<h3>
+							<p className='blurb'>
+								PomoPets is an incentivized Pomodoro timer. Begin your focus
+								session with a mysterious, hatchable egg and upon completing
+								your focus session, you are rewarded with a cute digital pet!{' '}
+							</p>
+							<br />
+							<p className='blurb'>
+								Pomodoro sessions are typically 25 minutes in length, with a 5
+								minute break between sessions. After every few sessions, take a
+								longer break to recharge and refresh your mind!{' '}
+							</p>
+						</h3>
+					</div>
+					<a href='https://pomopets.netlify.app/timers'>
+						<img
+							src={egg}
+							alt='egg-on-homepage'
+							className='egg'
+							width='400px'
+							height='400px'
+						/>
+					</a>
+				</div>
+			</div>
+		</>
+	);
 }
